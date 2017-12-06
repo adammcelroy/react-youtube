0d7edd81d1e9cad4d0360d367f9c7f519c5bb9d4
Adam McElroy <contact@adammcelroy.com>
2017-12-06 21:18:02 +0000

    Commit to redeploy

--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ import './scss/main.css';
 import Router from './components/Router';
 import reducers from './reducers';
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnhancers(applyMiddleware(
-	promiseMiddleware,
-)));
+const store = createStore(reducers, composeEnhancers(applyMiddleware(promiseMiddleware)));
 
 ReactDOM.render(
 	<Provider store={store}>
